6f8ab4a0f532267836e22fc94c269153e2ee6cde
NeilKleistGao <neilkleistgao@163.com>
2020-10-02 23:00:45 +0800

    Site updated: 2020-10-02 23:00:45

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,25 +1,38 @@
-// fancybox js
-let fancyTimer = setInterval(function(){
-  if(!window.$){
-    return;
-  }
-  $(document).ready(function() {
-    $(".markdown-body img").each(function () {
-      if($(this).parent().get(0).tagName.toLowerCase() === "a") {
-        return;
-      }
-      // $(this).attr("data-fancybox", "gallery"); // if you add 'data-fancybox', img will display after showed
-      var element = document.createElement("a");
-      $(element).attr("data-fancybox", "gallery");
-      // 判断是否启用了lazyload图片懒加载
-      if ($(this).attr("data-original")) {
-        $(element).attr("href", $(this).attr("data-original"));
-      } else {
-        $(element).attr("href", $(this).attr("src"));
-      }
-      $(this).wrap(element);
+// reference: https://fancyapps.com/fancybox/3/docs/
+$(document).ready(function() {
+    $('img').each(function() {
+        if ($(this).parent().hasClass('fancybox')) return;
+        if ($(this).hasClass('nofancybox')) return;
+        var alt = this.alt;
+        $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" data-fancybox="gallery"' + ' data-caption="' + alt + '" class="fancybox"></a>');
     });
+    $(this).find('.fancybox').each(function(){
+        $(this).attr('rel', 'article');
+    });
+});
+$(document).ready(function() {
+    $('[data-fancybox=gallery]').fancybox({
+        buttons : [
+          'zoom',
+          //'share',
+          'slideShow',
+          'fullScreen',
+          //'download',
+          'thumbs',
+          'close'
+        ],
+        // Transition effect between slides
+        //
+        // Possible values:
+        //   false            - disable
+        //   "fade'
+        //   "slide'
+        //   "circular'
+        //   "tube'
+        //   "zoom-in-out'
+        //   "rotate'
+        //
+        transitionEffect: "slide",
+    });
+});
 
-    clearInterval(fancyTimer);
-  });
-}, 10);
